f2195748557c051a254d1cb87617679604a98aef
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2023-07-12 00:27:39 +0200

    trash and base materials

--- Assets/Items/IngredientsScriptable.cs
+++ Assets/Items/IngredientsScriptable.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new Ingredient", menuName = "Ingredients")]
 public class IngredientScriptable : ScriptableObject
 {
-    [SerializeField] string ItemName;
+    [SerializeField] string fullName;
     [SerializeField] GameObject model3D;
     [SerializeField] List<IngredientScriptable> myIngredients = new List<IngredientScriptable>();
 }
