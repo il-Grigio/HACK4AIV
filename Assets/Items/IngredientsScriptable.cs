3d8e9c355360aaa0ed4372455f4512cfc0a3fd6f
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2023-07-11 23:48:40 +0200

    Materials

---
+++ Assets/Items/IngredientsScriptable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "new Ingredient", menuName = "Ingredients")]
+public class IngredientScriptable : ScriptableObject
+{
+    [SerializeField] string ItemName;
+    [SerializeField] GameObject model3D;
+    [SerializeField] List<IngredientScriptable> myIngredients = new List<IngredientScriptable>();
+}
---
+++ Assets/Machines/MachineScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum MachineType { }
+public class MachineScript : MonoBehaviour
+{
+    [SerializeField] MachineType MachineType;
+    
+    //TODO check if player in range and looking at the machine to interract
+}
