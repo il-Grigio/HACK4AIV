78e76a3416c530067474e63bdd20f53e49a9bb68
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2023-07-12 23:50:57 +0200

    finished threadmill

--- Assets/Machines/MachineScript.cs
+++ Assets/Machines/MachineScript.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public enum MachineType { 
     Decrafting,
     DecraftingSpam,
-    Crafting
+    Crafting,
+    Table,
+    Threadmill
 }
 public abstract class MachineScript : MonoBehaviour
 {
