fe600ac310292f0c8be0444c3b433f90474e86c2
Alessandro Varisco <82876230+il-Grigio@users.noreply.github.com>
2023-07-13 00:15:03 +0200

    Table

---
+++ Assets/Machines/Table.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Table : MachineScript
+{
+    public override bool Interact() {
+
+        return false;
+    }
+}
