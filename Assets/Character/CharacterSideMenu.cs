695e25ddeb55cba703f10d05b7867292fd1b2103
Matteo Poli <matteopoli2001@gmail.com>
2023-07-14 12:58:52 +0200

    UI Implementation

--- Assets/Character/CharacterSideMenu.cs
+++ Assets/Character/CharacterSideMenu.cs
@@ -7,15 +7,28 @@ using UnityEngine;
 [AddComponentMenu("TopDown Engine/Character/Abilities/SideMenu")]
 public class CharacterSideMenu : CharacterAbility {
     protected float _verticalMovement;
+    private UIAnimationTrigger trigger;
+    private CharacterMovement charMoveScript;
+    private bool isToggled = false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        trigger = GameObject.Find("RecipesMenu").GetComponent<UIAnimationTrigger>();
+        charMoveScript = GetComponent<CharacterMovement>();
+    }
+
     protected override void HandleInput() {
         // here as an example we check if we're pressing down
         // on our main stick/direction pad/keyboard
         if (_inputManager.ReloadButton.State.CurrentState == MMInput.ButtonStates.ButtonDown) {
-            //Your Method
-
+            trigger.TriggerAnimation();
+            isToggled = !isToggled;
+            
+            PermitAbility(isToggled);
+            charMoveScript.InputAuthorized = !isToggled;
+            
         }
-
-        //_horizontalMovement = _horizontalInput;
         _verticalMovement = _verticalInput;
     }
 }
