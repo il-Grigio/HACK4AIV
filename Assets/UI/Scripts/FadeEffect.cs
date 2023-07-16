ed27dd4de7968729736c176e52307b8e9d058c0a
BlueTonio <82876244+BlueTonio@users.noreply.github.com>
2023-07-16 16:35:45 +0200

    end lose game done

---
+++ Assets/UI/Scripts/FadeEffect.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+public class FadeEffect : MonoBehaviour
+{
+    [SerializeField] float fadeSpeed = 5f;
+    TextMeshProUGUI textMeshProUGUI;
+    TextMeshPro textMeshPro;
+    Image image;
+    [SerializeField] UnityEvent finishFadeIN;
+    [SerializeField] UnityEvent finishFadeOUT;
+
+    [SerializeField] bool fadingIn = false;
+    float counter;
+    float risingVal;
+    float oldRisingVal;
+    void Start()
+    {
+        textMeshProUGUI = GetComponent<TextMeshProUGUI>();
+        image = GetComponent<Image>();
+        textMeshPro = GetComponent<TextMeshPro>();
+    }
+
+    void Update()
+    {
+        oldRisingVal = risingVal;
+
+        counter += (fadingIn ? Time.deltaTime : -Time.deltaTime);
+
+        counter = Mathf.Clamp(counter, 0, fadeSpeed);
+        risingVal = counter / fadeSpeed;
+        risingVal = Mathf.Clamp01(risingVal);
+        if (textMeshProUGUI) textMeshProUGUI.alpha = risingVal;
+        if (textMeshPro) textMeshPro.alpha = risingVal;
+        if (image) image.color = new Color(image.color.r, image.color.g, image.color.b, risingVal);
+        if (oldRisingVal != 1 && risingVal == 1)
+        {
+            finishFadeIN.Invoke();
+        }
+        if (oldRisingVal != 0 && risingVal == 0)
+        {
+            finishFadeOUT.Invoke();
+        }
+
+    }
+    public void FadeIn()
+    {
+        fadingIn = true;
+    }
+    public void FadeOut()
+    {
+        fadingIn = false;
+    }
+
+}
