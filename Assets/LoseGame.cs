b31bf19298ff5b96ec016f4b493f3f76648bc93c
BlueTonio <82876244+BlueTonio@users.noreply.github.com>
2023-07-17 11:02:42 +0200

    end game fix

--- Assets/LoseGame.cs
+++ Assets/LoseGame.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoseGame : MonoBehaviour
 {
     [SerializeField] GameObject loseGameScreen;
     [SerializeField] GameObject thanksForPlayingObj;
     [SerializeField] FadeEffect fade;
+    bool gameFinish = false;
+    float timer = 5;
+    float currentTimer = 0;
+    float speed = 1;
 
     void Start()
     {
@@ -16,11 +21,21 @@ public class LoseGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameFinish)
+        {
+            currentTimer += speed * Time.deltaTime;
+            if (currentTimer >= timer)
+            {
+                currentTimer = 0;
+                SceneManager.LoadScene("AntonioScena");
+            }
+        }
+       
     }
 
     public void EndLoseGame() 
     {
+        gameFinish = true;
         fade.gameObject.SetActive(true);
         fade.FadeIn();
         loseGameScreen.SetActive(true);
--- Assets/WinGame.cs
+++ Assets/WinGame.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinGame : MonoBehaviour
 {
     [SerializeField] GameObject winGameScreen;
     [SerializeField] GameObject thanksForPlayingObj;
     [SerializeField] FadeEffect fade;
-
+    bool gameFinish = false;
+    
+    float timer = 5;
+    float currentTimer = 0;
+    float speed = 1;
     void Start()
     {
         
@@ -16,10 +21,19 @@ public class WinGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameFinish)
+        {
+            currentTimer += speed*Time.deltaTime;
+            if (currentTimer >= timer)
+            {
+                currentTimer = 0;
+                SceneManager.LoadScene("AntonioScena");
+            }
+        }
     }
     public void EndWinGame()
     {
+        gameFinish = true;
         fade.gameObject.SetActive(true);
         fade.FadeIn();
         winGameScreen.SetActive(true);
